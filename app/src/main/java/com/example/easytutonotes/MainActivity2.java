8fddda589a0336fa1ccbc918690629ff2d6efc9f
evilalpha0007 <akarandikar33@gmail.com>
2022-08-11 01:08:18 +0530

    my project

---
+++ app/src/main/java/com/example/easytutonotes/MainActivity2.java
@@ -0,0 +1,35 @@
+package com.example.easytutonotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    TextView appname;
+    LottieAnimationView lott;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        appname=findViewById(R.id.appnames);
+        lott=findViewById(R.id.lottie);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent isplash=new Intent(MainActivity2.this,MainActivity.class);
+                startActivity(isplash);
+                finish();
+            }
+        },3000);
+    }
+}
